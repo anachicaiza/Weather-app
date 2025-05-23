de2cdd4b11e08ff8329d7c08a32161b9e3eca51a
Ana Chicaiza <anachicaiza@icloud.com>
2025-05-23 17:38:59 +0100

    JS
    Function created to update date and time on website

--- src/script.js
+++ src/script.js
@@ -5,5 +5,37 @@ function search(event) {
   cityElement.innerHTML = searchInput.value;
 }
 
+function formatDay(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  if (minutes < 10) {
+    minutes = `0${minutes};`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours};`;
+  }
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
+
+let actualDateElement = document.querySelector("#current-date");
+let actualDate = new Date();
+
+actualDateElement.innerHTML = formatDay(actualDate);
